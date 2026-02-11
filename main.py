403fe10572b63b9323770626846d1096ead9c678
abbos-khamidov <adamsmidov02@gmail.com>
2026-02-11 19:17:09 +0500

    added kb for next and prev

--- main.py
+++ main.py
@@ -4,11 +4,88 @@ from aiogram.types import Message, CallbackQuery, InlineKeyboardMarkup, InlineKe
 from aiogram.filters import CommandStart
 from aiogram.utils.keyboard import InlineKeyboardBuilder
 
-TOKEN = ''
+TOKEN = '8422643227:AAFppSmJFxxW1JA36YJGs91uSuuv6Xtv6nY'
 bot = Bot(token=TOKEN)
 dp = Dispatcher()
 
 
+products = [
+    {
+        'name': 'Nike Air',
+        'description': 'Good sneakers for running',
+        'price': 120,
+        'photo': 'https://m.media-amazon.com/images/I/71Ru+PAVZCL._AC_SL1000__.jpg'
+    },
+    {
+        'name': 'Adidas Pro',
+        'description': 'Famous sneakers for street',
+        'price': 120,
+        'photo': 'https://www.misterrunning.com/images/2025-media-02/adidas-adizero-adios-pro-4-scarpe-da-running-uomo-cloud-white-jr1094-A.jpg'
+    },
+    {
+        'name': 'Puma Street',
+        'description': 'Best choice for runners and who loves puma',
+        'price': 210,
+        'photo': 'https://cdn.afew-store.com/assets/48/487132/2400/puma-h-street-og-white-403692-05-footwear%20%3E%20sneaker-packshots-0.jpg'
+    }
+]
+
+
+user_positions = {}
+user_cart = {}
+
+def product_keyboard(index: int):
+    builder = InlineKeyboardBuilder()
+    builder.row(
+        InlineKeyboardButton(text='Prev', callback_data='prev'),
+        InlineKeyboardButton(text='Next', callback_data='next'),
+    )
+    builder.row(
+        InlineKeyboardButton(text='To cart', callback_data='add')
+    )
+    return builder.as_markup()
+
+def product_caption(index: int):
+    product = products[index]
+    return (
+        f'{product['name']}\n'
+        f'{product['description']}\n'
+        f'${product['price']}\n'
+        f'\nProduct {index + 1} from {len(products)}'
+    )
+
+@dp.message(CommandStart())
+async def start_handler(message: Message):
+    user_positions[message.from_user.id] = 0
+    index = 0
+    await message.answer_photo(
+        photo = products[index]['photo'],
+        caption=product_caption(index),
+        reply_markup=product_keyboard(index)
+    )
+
+@dp.callback_query(F.data.in_(['next', 'prev']))
+async def navigate_handler(callback: CallbackQuery):
+    user_id = callback.from_user.id
+    index = user_positions.get(user_id, 0)
+
+    if callback.data == 'next':
+        index = (index + 1) % len(products)
+    else:
+        index = (index - 1) % len(products)
+
+    user_positions[user_id] = index
+
+    await callback.message.edit_media(
+        media = {
+            'type': 'photo',
+            'media': products[index]['photo'],
+            'caption': product_caption(index),
+        },
+        reply_markup=product_keyboard(index)
+    )
+
+    await callback.answer()
 
 async def main():
     await dp.start_polling(bot)
